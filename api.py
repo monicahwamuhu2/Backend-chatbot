bdb66e2f740133e9bc904d9d259cc44ca6a68750
monicahwamuhu2 <monicahwamuhu2@gmail.com>
2025-02-17 08:16:16 +0300

    final commit

--- api.py
+++ api.py
@@ -21,7 +21,7 @@ app = FastAPI()
 # Add CORS middleware to allow requests from the frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Allow only this frontend
+    allow_origins=["https://frontend-deploy-tawny-one.vercel.app/"],  # Allow only this frontend
     allow_credentials=True,
     allow_methods=["*"],  # Allow all HTTP methods (GET, POST, etc.)
     allow_headers=["*"],  # Allow all headers
